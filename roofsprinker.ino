0b3f382063b6c58f2c4da13f9e77c70720d6d9ac
UEOcli <oclinaria.nathanielakaisan@ue.edu.ph>
2026-04-20 20:00:16 +0800

    Update roofsprinker.ino

--- roofsprinker.ino
+++ roofsprinker.ino
@@ -6,8 +6,8 @@
 #define DHTTYPE DHT11
 #define PUMP_PIN 8
 
-#define HUMIDITY_THRESHOLD 50.0  // for testing only
-#define TEMP_THRESHOLD 37.0      // for testing only
+#define HUMIDITY_THRESHOLD 60.0  // for testing only
+#define TEMP_THRESHOLD 36.0      // for testing only
 
 DHT dht(DHTPIN, DHTTYPE);
 LiquidCrystal_I2C lcd(0x27, 16, 2);
@@ -37,9 +37,8 @@ void loop() {
     lcd.setCursor(0, 1);
     lcd.print("Sensor Error  ");
     delay(2000);
-    return;
   }
-
+  //displays the temperature and humidity on the LCD screen
   lcd.setCursor(0, 0);
   lcd.print("Temp: ");
   lcd.print(temperature, 1);
@@ -49,20 +48,13 @@ void loop() {
   lcd.print("Hum: ");
   lcd.print(humidity, 1);
   lcd.print("%    ");
-
-  if (temperature > TEMP_THRESHOLD) {
+  //controls the pump based on the humidity and temperature readings
+  if (temperature > TEMP_THRESHOLD && humidity <= HUMIDITY_THRESHOLD) {
     digitalWrite(PUMP_PIN, HIGH);
     Serial.println("Pump ON");
   } else {
     digitalWrite(PUMP_PIN, LOW);
     Serial.println("Pump OFF");
   }
-
-//Remove when needed for proper demonstration
-/*#define TEMP_THRESHOLD_PRACTICAL 38.0
-// humidity not used
-if (temperature > TEMP_THRESHOLD_PRACTICAL) {
-  digitalWrite(PUMP_PIN, HIGH);
-}*/
   delay(2000);                 
 }
